d20cee28651e845555ed00924a496c73d343e532
RochesWebStudio <rocheswebstudio@gmail.com>
2026-03-26 11:53:52 +0100

    fixe(methodes): application plus rigoureuse du principe SOLID.

--- js/jeux/puissance4/PlateauP4.js
+++ js/jeux/puissance4/PlateauP4.js
@@ -22,4 +22,24 @@ export default class PlateauP4 {
             }
         }
     }
+
+    colonneJouable (colonne) {
+        return (this.grille[0][colonne] == JETON_VIDE);
+    }
+
+    estGrillePleine () {
+        for (let colonne = 0; colonne < COLONNES; colonne++) {
+            if (this.colonneJouable(colonne)) return false;
+        }
+        return true;
+    }
+
+    ligneJouable (colonne) {
+        for (let ligne = (LIGNES - 1); ligne >= 0; ligne--) {
+            if (this.grille[ligne][colonne] == JETON_VIDE) {
+                return ligne;
+            }
+        }
+        return false;
+    }
 }
